28e293c7038f44ce103fd96cc21bd64d0c668734
Lennart-Meyer <lennart.meyer@student.jade-hs.de>
2020-10-19 14:42:28 +0200

    initial Summery

---
+++ CatiaAnbindung/CatiaAnbindung/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatiaAnbindung
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double m  ;  //m=Modul
+            Double p  ;  //p=Teilung
+            Double d  ;  //d=Teilkreisdurchmesser
+            Double da ; //da=Kopfkreisdurchmesser
+            Double df ; //df=Frußkreisdurchmesser
+            Double z  ;  //z=Zähnezahl
+            Double h  ;  //h=Zahnhöhe
+            Double ha ; //ha=Zahnkopfhöhe
+            Double hf ; //hf=Zahnfußhöhe
+            Double c  ;  //c=Kopfspiel
+            Double a  ;  //a=Achsenabstand
+
+            Console.WriteLine("Wollen sie die Modul und Teilkreisdurchmesser vorgeben press 1");
+            Console.WriteLine("Wollen sie die Msduodul und Zähnezahl vorgeben press 2");
+            Console.WriteLine("Wollen sie die Zähnezahl und Teilkreisdurchmesser vorgeben press 3");
+            Console.WriteLine("und bestätigen sie ihre Eingabe mit Enter");
+
+            int ii= Convert.ToInt32(Console.Readkey());
+           
+            if (ii == 1)
+            {
+                Console.WriteLine("Bitte geben sie das Modul ein.");
+                m = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Bitte geben sie den Teilkreisdurchmesser in mm an");
+                d = Convert.ToDouble(Console.ReadLine());
+
+            }
+            
+            
+            
+            
+            
+            
+            
+            Console.WriteLine(ii);
+            Console.ReadKey();
+
+          
+        }
+    }
+}
